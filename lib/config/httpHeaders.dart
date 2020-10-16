5a732996288995ceba6e5c00ce66900e06032514
laibinzhi <444239383@qq.com>
2020-10-16 14:38:11 +0800

    模拟请求http

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,18 @@
+const httpHeaders = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8',
+  'Connection': 'keep-alive',
+  'Content-Type': 'application/json',
+  'Cookie':
+      '_ga=GA1.2.1737780631.1600244561; LF_ID=1600244561691-6252861-3296234; gksskpitn=ad920501-3996-4d49-b2f0-b9a9685484f7; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22174960236981e1-0c4b9b198640da-15356251-1296000-17496023699e79%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E4%BB%98%E8%B4%B9%E5%B9%BF%E5%91%8A%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.baidu.com%2Fother.php%22%2C%22%24latest_landing_page%22%3A%22https%3A%2F%2Ftime.geekbang.org%2F%3Futm_campaign%3Dguanwang%26utm_source%3Dbaidu-ad%26utm_medium%3Dppzq-pc%26utm_content%3Dtitle%26utm_term%3Dbaidu-ad-ppzq-title%22%2C%22%24latest_utm_source%22%3A%22baidu-ad%22%2C%22%24latest_utm_medium%22%3A%22ppzq-pc%22%2C%22%24latest_utm_campaign%22%3A%22guanwang%22%2C%22%24latest_utm_content%22%3A%22title%22%2C%22%24latest_utm_term%22%3A%22baidu-ad-ppzq-title%22%7D%2C%22%24device_id%22%3A%22174960236981e1-0c4b9b198640da-15356251-1296000-17496023699e79%22%7D; Hm_lvt_59c4ff31a9ee6263811b23eb921a5083=1600244562,1602828121; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1600244562,1602828121; GRID=c0178f9-e5a4764-d2c8afc-f3f5a8b; _gid=GA1.2.212711263.1602828122; gk_process_ev={%22count%22:2%2C%22referrer%22:%22https://time.geekbang.org/%22%2C%22target%22:%22page_course_list%22%2C%22utime%22:1602829204595%2C%22referrerTarget%22:%22page_course_list%22}; acw_tc=2760825516028301631552580e4a1c6c17f98961bdfaef787bcc729bc89621; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1602830163|1602828120; Hm_lpvt_59c4ff31a9ee6263811b23eb921a5083=1602830163; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1602830163; _gat=1',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Referer':
+      'https://time.geekbang.org/?utm_campaign=guanwang&utm_source=baidu-ad&utm_medium=ppzq-pc&utm_content=title&utm_term=baidu-ad-ppzq-title',
+  'Sec-Fetch-Dest': 'empty',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../config/httpHeaders.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,38 +8,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎您来到美好人间高级会所';
+  String showText = '还没有请求数据';
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('美好人间'),
+          title: Text('请求远程数据'),
         ),
-        body: Container(
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0),
-                    labelText: '美女类型',
-                    helperText: '请输入你喜欢的类型'),
-                autofocus: false,
-              ),
               RaisedButton(
                 onPressed: () {
-                  _choiceAction();
+                  _jike();
                 },
-                child: Text('选择完毕'),
+                child: Text('请求数据'),
               ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              )
+              Text(showText)
             ],
           ),
         ),
@@ -46,31 +34,22 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _choiceAction() {
-    print('开始选择您喜欢的类型..............');
-    if (typeController.text.toString() == '') {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('美女类型不能为空'),
-        ),
-      );
-    } else {
-      getHttp(typeController.text.toString()).then((value) {
-        setState(() {
-          showText = value;
-        });
+  void _jike() {
+    print('开始向极客时间请求数据.................');
+    getHttp().then((value) {
+      setState(() {
+        showText = value['data'].toString();
       });
-    }
+    });
   }
 
-  Future getHttp(String TypeText) async {
+  Future getHttp() async {
     try {
       Response response;
-      var data = {'name': TypeText};
-      response = await Dio().get(
-          "https: //www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
-          queryParameters: data);
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      response = await dio.get("https://time.geekbang.org/serv/v2/explore/all");
+      print(response);
       return response.data;
     } catch (e) {
       return print(e);

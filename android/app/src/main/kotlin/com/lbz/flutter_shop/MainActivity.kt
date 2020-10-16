09de701e23fb7b6be5fcd2dc565a5edec3636128
laibinzhi <444239383@qq.com>
2020-10-16 10:17:26 +0800

    底部导航栏切换效果

---
+++ android/app/src/main/kotlin/com/lbz/flutter_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lbz.flutter_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'pages/index_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        title: '百姓生活',
+        //去除右上角debug
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.pink,
+        ),
+        home: IndexPage(),
+      ),
+    );
+  }
+}

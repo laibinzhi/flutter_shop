d6a01f3f243c7dbb612c0c6961a48cc43f90e8b4
laibinzhi <444239383@qq.com>
2020-10-19 16:03:59 +0800

    首页_楼层区域的编写

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,8 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin {
   String homePageContent = '正在获取数据';
 
   @override
@@ -41,22 +42,29 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
             String leaderPhone = data['data']['shopInfo']['leaderPhone']; //店长电话
             List<Map> recommendList =
                 (data['data']['recommend'] as List).cast();
+            String floor1Title = data['data']['floor1Pic']['PICTURE_ADDRESS'];
+            String floor2Title = data['data']['floor2Pic']['PICTURE_ADDRESS'];
+            String floor3Title = data['data']['floor3Pic']['PICTURE_ADDRESS'];
+
+            List<Map> floor1 = (data['data']['floor1'] as List).cast();
+            List<Map> floor2 = (data['data']['floor2'] as List).cast();
+            List<Map> floor3 = (data['data']['floor3'] as List).cast();
 
             return SingleChildScrollView(
               child: Column(
                 children: [
                   SwiperDiy(swiperDataList: swiper),
-                  TopNavigator(
-                    navigatorList: navgatorList,
-                  ),
-                  AdBanner(
-                    adPicture: adPicture,
-                  ),
+                  TopNavigator(navigatorList: navgatorList),
+                  AdBanner(adPicture: adPicture),
                   LeaderPhone(
                       leaderImage: leaderImage, leaderPhone: leaderPhone),
-                  Recommend(
-                    recommendList: recommendList,
-                  )
+                  Recommend(recommendList: recommendList),
+                  FloorTitle(picture_address: floor1Title),
+                  FloorContent(floorGoodsList: floor1),
+                  FloorTitle(picture_address: floor2Title),
+                  FloorContent(floorGoodsList: floor2),
+                  FloorTitle(picture_address: floor3Title),
+                  FloorContent(floorGoodsList: floor3),
                 ],
               ),
             );
@@ -262,6 +270,72 @@ class Recommend extends StatelessWidget {
           _titleWidget(),
           _recommedList(),
         ],
+      ),
+    );
+  }
+}
+
+//楼层标题
+class FloorTitle extends StatelessWidget {
+  final String picture_address;
+
+  const FloorTitle({Key key, this.picture_address}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Image.network(picture_address),
+    );
+  }
+}
+
+//楼层商品列表
+class FloorContent extends StatelessWidget {
+  final List floorGoodsList;
+
+  const FloorContent({Key key, this.floorGoodsList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [_firstRow(), _otherGoods()],
+      ),
+    );
+  }
+
+  Widget _firstRow() {
+    return Row(
+      children: [
+        _goodsItem(floorGoodsList[0]),
+        Column(
+          children: [
+            _goodsItem(floorGoodsList[1]),
+            _goodsItem(floorGoodsList[2]),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _otherGoods() {
+    return Row(
+      children: [
+        _goodsItem(floorGoodsList[3]),
+        _goodsItem(floorGoodsList[4]),
+      ],
+    );
+  }
+
+  Widget _goodsItem(Map goods) {
+    return Container(
+      width: ScreenUtil().setWidth(375),
+      child: InkWell(
+        onTap: () {
+          print('点击了楼层商品');
+        },
+        child: Image.network(goods['image']),
       ),
     );
   }

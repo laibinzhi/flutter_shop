f0f3f25f62b793c1d75a029b77b4cd9dc4f313c0
laibinzhi <444239383@qq.com>
2020-10-20 11:56:05 +0800

    Provide状态管理基础

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,45 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+import '../provide/counter.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('购物车页面'),
+        body: Center(
+      child: Column(
+        children: [Number(), MyButton()],
       ),
+    ));
+  }
+}
+
+class Number extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 200),
+      child: Provide<Counter>(
+        builder: (context, child, counter) {
+          return Text(
+            '${counter.value}',
+            style: Theme.of(context).textTheme.display1,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        Provide.value<Counter>(context).increment();
+      },
+      child: Text('递增'),
     );
   }
 }
